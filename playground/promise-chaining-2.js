933e44df9e0b606b7788f5ef6c71715c27677a61
Dikmit02 <dikshamittal21297@gmail.com>
2019-08-13 17:08:16 +0530

    async requests

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,11 +1,25 @@
 require('../src/db/mongoose')
 const Task = require('../src/models/task')
 
-Task.findByIdAndDelete('5d5167cb27bc9b1bfd65a80f').then((task) => {
-    console.log(task)
-    return Task.countDocuments({ completed: false })
-}).then((result) => {
-    console.log(result)
-}).catch((e) => {
-    console.log(e)
-})
+// Task.findByIdAndDelete('5d5167cb27bc9b1bfd65a80f').then((task) => {
+//     console.log(task)
+//     return Task.countDocuments({ completed: false })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+const deleteTaskAndCount=async(id)=>{
+    const delete_user=await Task.findByIdAndDelete(id)
+    const count=await Task.countDocuments({completed:false})
+    return count
+ }
+ deleteTaskAndCount('5d51cd0b08fefb1d9742fd9e')
+ .then((count)=>{
+     console.log(count)
+
+ })
+ .catch((e)=>[
+     console.log(e)
+ ])
--- playground/promise-chaining.js
+++ playground/promise-chaining.js
@@ -1,11 +1,25 @@
 require('../src/db/mongoose')
 const User = require('../src/models/user')
 
-User.findByIdAndUpdate('5d51c6537f5d2219de1b9555', { age: 1 }).then((user) => {
+// User.findByIdAndUpdate('5d51c6537f5d2219de1b9555', { age: 1 }).then((user) => {
+//     console.log(user)
+//     return User.countDocuments({ age: 1 })
+// }).then((result) => {
+//     console.log(result)
+// }).catch((e) => {
+//     console.log(e)
+// })
+
+
+const UpadteageandCount=async(id,age)=>{
+    const user=await User.findByIdAndUpdate(id,{age})
     console.log(user)
-    return User.countDocuments({ age: 1 })
-}).then((result) => {
-    console.log(result)
-}).catch((e) => {
+    const count=await User.countDocuments({age})
+    return count
+}
+
+UpadteageandCount('5d51c6537f5d2219de1b9555',2).then((count)=>{
+    console.log(count)
+}).catch((e)=>{
     console.log(e)
 })
