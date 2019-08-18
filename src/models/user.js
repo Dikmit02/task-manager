38ce42ba09ff15d5b4720353c2a86204ff288bde
Dikmit02 <dikshamittal21297@gmail.com>
2019-08-18 19:58:00 +0530

    jwt

--- src/models/user.js
+++ src/models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
 const  bcrypt=require('bcrypt')
+const jwt = require('jsonwebtoken')
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -38,12 +39,19 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number')
             }
         }
-    }
+    },
+    tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 
 })
 
 
 //craeting the new method
+//statics is for model
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
 
@@ -58,6 +66,16 @@ userSchema.statics.findByCredentials = async (email, password) => {
     }
 
     return user
+}
+//method is for instances
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+    const token = jwt.sign({ _id: user._id.toString() }, 'thisismynewcourse')
+
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+
+    return token
 }
 
 
